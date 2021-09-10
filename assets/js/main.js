c99a417cac9ecc4367c59f8d50049ac78f1354f7
kensuke <88492495+kensuke537@users.noreply.github.com>
2021-09-10 10:43:08 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '金港','きんこう'
         },
         stage2: {
           q1: 'いいい',
